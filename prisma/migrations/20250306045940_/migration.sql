3ef266a13134d930fb84156f78bf5d5979dfa873
haochen <sumtsui@gmail.com>
2025-03-06 13:00:46 +0800

    use neon so demo can work in vercel

---
+++ prisma/migrations/20250306045940_/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "pieces" INTEGER NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "archivedAt" TIMESTAMP(3),
+    "categoryId" TEXT,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_userId_name_key" ON "Category"("userId", "name");
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -14,12 +14,7 @@ export interface SessionPayload extends JWTPayload {
   userId: string;
 }
 
-export const MAX_FILE_SIZE_ALLOWED_MB = 15;
-export const MAX_FILE_SIZE_FOR_UPLOAD_MB = 0.5;
-export const FREE_TRAIL_IMAGE_ANALYSIS_COUNT_PER_MONTH = 3;
-export const MEMBERSHIP_IMAGE_ANALYSIS_COUNT_PER_MONTH = 10;
-export const FREE_TRAIL_ITEMS_LIMIT = 20;
-export const MEMBERSHIP_ITEMS_LIMIT = 3000;
+export const FREE_TRAIL_ITEMS_LIMIT = 13;
 
 export const ERROR_FREE_TRAIL_ITEM_LIMIT =
   "Free trail items limit reached. To add more items, please consider joining membership.";
